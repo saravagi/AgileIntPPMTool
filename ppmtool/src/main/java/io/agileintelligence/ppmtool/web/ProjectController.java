fde8922e440c2d20d9e6975aa005ba855de43ef9
saravagi <ayush.saravagi@gmail.com>
2020-01-14 12:12:52 +0530

    Modifying the error

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -23,7 +28,11 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
         if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Object",HttpStatus.BAD_REQUEST);
+            Map<String, String> errMap = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()){
+                errMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String,String>>(errMap,HttpStatus.BAD_REQUEST);
         }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
