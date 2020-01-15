a53b76fd1e402b74a448c4148735f654fe743928
saravagi <ayush.saravagi@gmail.com>
2020-01-15 13:45:12 +0530

    find all project function completed.

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -3,8 +3,16 @@ package io.agileintelligence.ppmtool.repositories;
 import io.agileintelligence.ppmtool.domain.Project;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends CrudRepository<Project, Long> {
    Project findByProjectIdentifier(String projectId);
    //where is the logic written for findByProjectIdentifier
     //findBy is only used is the coloumn is unique
+
+   @Override
+   Iterable<Project> findAll();
+   // we use iterable when we are getting a list in return . Here we are getting Json objects list so we use iterable.
+
+
 }
--- ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -43,6 +43,10 @@ public class ProjectController {
         return new ResponseEntity<Project>(project, HttpStatus.OK);
 
     }
+    @GetMapping("/all")
+    public Iterable<Project> findAllProjects(){
+        return projectService.findAllProjects();
+    }
 
 
 
