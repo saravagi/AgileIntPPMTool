62520789033870e6f0f2524356eb76d65106d5b1
saravagi <ayush.saravagi@gmail.com>
2020-01-18 14:47:28 +0530

    AddProject created

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,22 +1,23 @@
-import React,{Component} from 'react';
-import './App.css';
-import Dashboard from './Components/Dashboard';
-import Header from './Layout/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./Components/Dashboard";
+import Header from "./Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./Components/Project/AddProject";
 
 class App extends Component {
-  render(){
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        <Header/> 
-        <Dashboard/>
-          
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
-  
 }
 
 export default App;
